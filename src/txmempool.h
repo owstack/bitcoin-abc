fa43bfd99597af7a641b4c5ea52a1f7277686165
hanchon <guillermo@bitprim.org>
2017-07-24 12:19:08 -0300

    indexes: adds additional address, spent and timestamp indexes
    Adds new bitcoin.conf configuration options for three new indexes:
    -addressindex=1
    -spentindex=1
    -timestampindex=1
    
    The addressindex records all changes to an address for retrieving txids, balances and
    unspent outputs for addresses. Changes are stored and sorted in block order. Both p2sh
    and p2pkh address types are supported. The index records two sets of key/value pairs.
    The first records all activity and is useful for viewing transaction history and
    all changes. The second is specifically for retrieving unspent outputs by address, and
    is smaller as values are removed once they are spent.
    
    The spentindex has multiple purposes and brings closer together inputs and outputs of
    transactions. The main purpose is to efficiently determine the address and amount of
    an input's previous output. The second purpose is to be able to determine which
    input spent an output.
    
    The timestampindex keeps track of timestamps with block hashes and is useful for searching
    blocks by date instead of by height. This is useful for a block explorer that will give
    search options by date. The index uses logical time correction to make sure that the
    results are sorted in block order. The logical time of a block is actual timestamp of the
    block, unless it is less than (earlier) the previous block's logical time, and in that
    case it is one second greater than the previous block's logical time.
    
    Includes logical time fix by Chethan Krishna

--- src/txmempool.h
+++ src/txmempool.h
@@ -6,6 +6,8 @@
 #ifndef BITCOIN_TXMEMPOOL_H
 #define BITCOIN_TXMEMPOOL_H
 
+#include "addressindex.h"
+#include "spentindex.h"
 #include "amount.h"
 #include "coins.h"
 #include "indirectmap.h"
@@ -543,6 +545,18 @@ private:
     typedef std::map<txiter, TxLinks, CompareIteratorByHash> txlinksMap;
     txlinksMap mapLinks;
 
+    typedef std::map<CMempoolAddressDeltaKey, CMempoolAddressDelta, CMempoolAddressDeltaKeyCompare> addressDeltaMap;
+    addressDeltaMap mapAddress;
+
+    typedef std::map<uint256, std::vector<CMempoolAddressDeltaKey> > addressDeltaMapInserted;
+    addressDeltaMapInserted mapAddressInserted;
+
+    typedef std::map<CSpentIndexKey, CSpentIndexValue, CSpentIndexKeyCompare> mapSpentIndex;
+    mapSpentIndex mapSpent;
+
+    typedef std::map<uint256, std::vector<CSpentIndexKey> > mapSpentIndexInserted;
+    mapSpentIndexInserted mapSpentInserted;
+
     void UpdateParent(txiter entry, txiter parent, bool add);
     void UpdateChild(txiter entry, txiter child, bool add);
 
@@ -577,6 +591,15 @@ public:
                       bool validFeeEstimate = true);
     bool addUnchecked(const uint256 &hash, const CTxMemPoolEntry &entry,
                       setEntries &setAncestors, bool validFeeEstimate = true);
+
+    void addAddressIndex(const CTxMemPoolEntry &entry, const CCoinsViewCache &view);
+    bool getAddressIndex(std::vector<std::pair<uint160, int> > &addresses,
+                         std::vector<std::pair<CMempoolAddressDeltaKey, CMempoolAddressDelta> > &results);
+    bool removeAddressIndex(const uint256 txhash);
+
+    void addSpentIndex(const CTxMemPoolEntry &entry, const CCoinsViewCache &view);
+    bool getSpentIndex(CSpentIndexKey &key, CSpentIndexValue &value);
+    bool removeSpentIndex(const uint256 txhash);
 
     void removeRecursive(
         const CTransaction &tx,
